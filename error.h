cfea0d81068b439c78f20b300817bb6f2b1c20ee
KillerOfTimeSK <62033140+KillerOfTimeSK@users.noreply.github.com>
2020-11-17 22:16:57 +0100

    Added error.h

---
+++ error.h
@@ -0,0 +1,16 @@
+#ifndef _ERROR_H
+#define _ERROR_H
+
+//Error codes
+#define ResultOK		0
+#define LexError		1
+#define SynError		2
+#define SemDefError		3
+#define SemTypeDefError		4
+#define SemTypeError		5
+#define SemFuncError		6
+#define SemError		7
+#define SemZeroError		9
+#define InternalError		99
+
+#endif
