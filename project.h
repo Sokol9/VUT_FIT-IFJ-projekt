d0b048e89fc9a7b1beea8e3ad98ce7c0bc21dc4a
haczech <74650591+haczech@users.noreply.github.com>
2020-11-21 13:18:49 +0100

    Update project.h
    Support key words and extended tokenTypes

---
+++ project.h
@@ -0,0 +1,76 @@
+#ifndef PROJECT_H
+#define PROJECT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+// maximalni delka identifikatoru
+#define MAX_LEN 1024
+#define KW_LEN 10
+
+// vycet vsech typu tokenu
+// operatory, separatory, zavorky, klicova slova, identifikator, literaly, EOF, UNKNOWN
+// US(underscore) je specialni promenna '_'
+//
+// operatory aritmeticke: ADD(+), SUB(-), MULT(*), DIV(/)
+// operatory logicke:     LT(<), LTEQ(<=), GT(>), GTEQ(>=), EQ(==), NEQ(!=)
+// operatory prirazeni:   ASG(=), DEF(:=)
+// separatory:            SEM(;), COM(,)
+// zavorky:               OBR('('), CBR(')'), OB('{'), CB('}')
+typedef enum {UNKNOWN=0, ADD, SUB, MULT, DIV, SEM, OBR, CBR, OB, CB, LT, LTEQ, GT, GTEQ, EQ, NEQ, ASG, DEF, COM, KW_ELSE, KW_FLOAT64, KW_FOR, KW_FUNC, KW_IF, KW_INT, KW_PACKAGE, KW_RETURN, KW_STRING, ID, US, INT_L, FLOAT_L, STRING_L, TOKEN_EOF} tokenType;
+
+// struktura tToken
+// typ urcen lexikalni analyzou, podle typu je rizena syntakticka analyza
+// atribut je precteny retezec ze vstupu
+typedef struct{
+	tokenType type;
+	char attr[MAX_LEN];
+}tToken;
+
+// vycet kriterii pro vyskyt EOLu pri lexikalni analyze
+// FORB ... EOL je zakazan
+// REQ  ... EOL je vyzadovan
+// OPT  ... EOL je volitelny
+typedef enum {FORB, REQ, OPT} eolFlag;
+
+//================================================================
+// binarni vyhledavaci strom pro klicova slova (tabulka klicovych slov)
+typedef struct keyWord{
+	char key[KW_LEN];
+	tokenType value;
+	struct keyWord *LPtr;
+	struct keyWord *RPtr;
+}*tKWPtr;
+
+// funkce obsluhujici praci s tabulkou klicovych slov
+//
+// inicializace tabulky klicovcyh slov
+//    provede vlozeni vsech klicovych slov do tabulky
+//    tabulka se inicializuje na zacatku behu programu a dale zustava nemenna
+tKWPtr KWInit();
+
+// vlozeni klicoveho slova do tabulky
+int KWInsert(tKWPtr *rootPtr, char *key, tokenType value);
+
+// vyhledani klicoveho slova v tabulce
+tokenType KWLookUp(tKWPtr rootPtr, char *key);
+
+// zruseni tabulky klicovych slov
+void KWDispose(tKWPtr *rootPtr);
+
+//================================================================
+// funkce pro vraceni tokenu na pozadani syntaktickeho analyzatoru
+// funkce vraci chybovou hodnotu nasledovne:
+//    1  ... vse v poradku
+//    0  ... lexikalni chyba, ale token je "validni" v ramci moznosti
+//    -1 ... token nevalidni, neocekavane odradkovani
+//    -2 ... token nevalidni, prilis dlouhy identifikator
+// parametry:
+//    ukazatel na token, pres ktery funkce token plni hodnotami
+//    ukazatel na eolFlag (v pripade, ze je REQ a funkce EOL precte na vstupu, meni ho na OPT)
+//    ukazatel na tabulku klicovych slov
+int getToken(tToken *token, eolFlag *ef, tKWPtr table);
+
+#endif
