dbf26cd8f8e9cbf5afeb859e6a242b2d0fcd15e7
haczech <74650591+haczech@users.noreply.github.com>
2020-11-24 18:50:57 +0100

    Update error.h

--- error.h
+++ error.h
@@ -2,16 +2,16 @@
 #define _ERROR_H
 
 //Error codes
-#define RESULTOK		0
-#define LEXERROR		1
-#define SYNERROR		2
-#define SEMDEFERROR		3
-#define SEMTYPEDEFERROR		4
-#define SEMTYPEERROR		5
-#define SEMFUNCERROR		6
-#define SEMERROR		7
-#define SEMZEROERROR		9
-#define INTERNALERROR		99
+#define RESULT_OK		0
+#define LEX_ERROR		1
+#define SYN_ERROR		2
+#define SEM_DEF_ERROR		3
+#define SEM_TYPEDEF_ERROR		4
+#define SEM_TYPE_ERROR		5
+#define SEM_FUNC_ERROR		6
+#define SEM_ERROR		7
+#define SEM_ZERO_ERROR		9
+#define INTERNAL_ERROR		99
 
 #endif
 
