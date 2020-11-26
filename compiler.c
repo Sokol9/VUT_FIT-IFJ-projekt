1975b4b8ffbdc697ea2833b9c5ca2c6acc96a282
Sokol9 <xsokol14@stud.fit.vutbr.cz>
2020-11-26 20:00:24 +0100

    repair some fail in includ symtable in compiler

--- compiler.c
+++ compiler.c
@@ -14,7 +14,7 @@ int main() {
 
 	//char *type[] = {"UNKNOWN", "ADD", "SUB", "MULT", "DIV", "SEM", "OBR", "CBR", "OB", "CB", "LT", "LTEQ", "GT", "GTEQ", "EQ", "NEQ", "ASG", "DEF", "COM", "KW_ELSE", "KW_FLOAT64", "KW_FOR", "KW_FUNC", "KW_IF", "KW_INT", "KW_PACKAGE", "KW_RETURN", "KW_STRING", "ID", "US", "INT_L", "FLOAT_L", "STRING_L", "TOKEN_EOF"};
 
-	rule_prog(&token, keyWords, &sucess);	
+	rule_prog(&token, GTable, keyWords, &sucess);	
 
 	
 	KWDispose(&keyWords);
